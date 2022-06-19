bd791b9026bc250d2d72a861ac27f2a8fe2d8b86
Abdulaziz <39178255+abdu4188@users.noreply.github.com>
2022-06-19 09:37:22 +0300

    🔥 remove boilerplate

--- src/router/index.js
+++ src/router/index.js
@@ -15,11 +15,6 @@ const routes = [
         name: 'Home',
         component: () => import('@/views/Home.vue'),
       },
-      {
-        path: '/about',
-        name: 'About',
-        component: () => import('@/views/About.vue'),
-      },
     ],
   },
 ];
