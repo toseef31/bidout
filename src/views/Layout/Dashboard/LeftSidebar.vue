ee2e61e165f1966d90ea33a4333fa9375d020436
toseef31 <peekinternational@gmail.com>
2022-07-13 18:27:31 +0500

    some css issues on all pages

--- src/views/Layout/Dashboard/LeftSidebar.vue
+++ src/views/Layout/Dashboard/LeftSidebar.vue
@@ -1,88 +1,90 @@
 <template>
 
-  <div class="desktop-view leftSide-module">
-   
-    <v-list
-      nav
-      dense
-      class="pa-0 main-menu" href=""
-    >
-      <v-list-item-group 
-        color="primary"
-      >
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-item-icon class="mr-6 mt-3">
-            <router-link :to="item.link" class="text-decoration-none"><v-icon v-text="item.icon"></v-icon></router-link>
-          </v-list-item-icon>
-
-            
-          <v-list-item-content class="text-left py-1">
-            <router-link :to="item.link" class="text-decoration-none"><v-list-item-title class="font-weight-bold" v-text="item.text" v-show="showSideBar"> 
-              
-            </v-list-item-title></router-link>
-            <span class="badge" :class="[ showSideBar ? 'msg-badge' : 'mobile-badge']" v-if="i == 3"
-              >{{showMsgCount}}</span>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-    <v-divider></v-divider>
-    <div class="sidebar-bottomMenu">
-      <v-list class="pa-0">
-        <v-list-group 
-          v-for="item in subitems"
-          :key="item.title"
-          v-model="item.active"
-          no-action
-          class="px-0 sub-menu-item"
-        >
-          <template v-slot:activator>
-            <v-list-item-icon class="mr-6 mt-3">
-              <v-icon v-text="item.action"></v-icon>
-            </v-list-item-icon>
-            <v-list-item-content  class="text-left py-1" v-show="showSideBar">
-              <v-list-item-title class="font-weight-bold" v-text="item.title"></v-list-item-title>
-            </v-list-item-content>
-          </template>
-
-          <v-list-item
-            v-for="child in item.items"
-            :key="child.title"
-          >
-            <v-list-item-icon class="mr-6 mt-3" v-show="!showSideBar">
-              <v-icon v-text="child.icon"></v-icon>
-            </v-list-item-icon>
-            <v-list-item-content class="text-left py-1" v-show="showSideBar">
-              <router-link :to="child.link" class="text-decoration-none">
-                <v-list-item-title class="font-weight-bold" v-text="child.title"></v-list-item-title>
-              </router-link>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-group>
-      </v-list>
+  <div class="leftSide-module">
+    <div class="desktop-view">
+     
       <v-list
         nav
-        dense 
-        class="pa-0"
+        dense
+        class="pa-0 main-menu" href=""
       >
-        <v-list-item-group
+        <v-list-item-group 
           color="primary"
-          class="px-0"
         >
-          <v-list-item>
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+          >
             <v-list-item-icon class="mr-6 mt-3">
-              <v-icon>mdi-logout {{showSideBar}}</v-icon>
+              <router-link :to="item.link" class="text-decoration-none"><v-icon v-text="item.icon"></v-icon></router-link>
             </v-list-item-icon>
 
-            <v-list-item-content class="text-left py-1" v-show="showSideBar">
-              <v-list-item-title class="font-weight-bold"  @click="signout">Logout</v-list-item-title>
+              
+            <v-list-item-content class="text-left py-1">
+              <router-link :to="item.link" class="text-decoration-none"><v-list-item-title class="font-weight-bold" v-text="item.text" v-show="showSideBar"> 
+                
+              </v-list-item-title></router-link>
+              <span class="badge" :class="[ showSideBar ? 'msg-badge' : 'mobile-badge']" v-if="i == 3"
+                >{{showMsgCount}}</span>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
       </v-list>
+      <v-divider></v-divider>
+      <div class="sidebar-bottomMenu">
+        <v-list class="pa-0">
+          <v-list-group 
+            v-for="item in subitems"
+            :key="item.title"
+            v-model="item.active"
+            no-action
+            class="px-0 sub-menu-item"
+          >
+            <template v-slot:activator>
+              <v-list-item-icon class="mr-6 mt-3">
+                <v-icon v-text="item.action"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content  class="text-left py-1" v-show="showSideBar">
+                <v-list-item-title class="font-weight-bold" v-text="item.title"></v-list-item-title>
+              </v-list-item-content>
+            </template>
+
+            <v-list-item
+              v-for="child in item.items"
+              :key="child.title"
+            >
+              <v-list-item-icon class="mr-6 mt-3" v-show="!showSideBar">
+                <v-icon v-text="child.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content class="text-left py-1" v-show="showSideBar">
+                <router-link :to="child.link" class="text-decoration-none">
+                  <v-list-item-title class="font-weight-bold" v-text="child.title"></v-list-item-title>
+                </router-link>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </v-list>
+        <v-list
+          nav
+          dense 
+          class="pa-0"
+        >
+          <v-list-item-group
+            color="primary"
+            class="px-0"
+          >
+            <v-list-item>
+              <v-list-item-icon class="mr-6 mt-3">
+                <v-icon>mdi-logout {{showSideBar}}</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content class="text-left py-1" v-show="showSideBar">
+                <v-list-item-title class="font-weight-bold"  @click="signout">Logout</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </div>
     </div>
   </div>
 </template>
