c8025b9f0c1b4736ae507dbdcc81322f0f627212
toseef31 <peekinternational@gmail.com>
2022-10-17 12:32:21 +0500

    dropzone

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -429,7 +429,6 @@ export default {
       this.showMsgBlock = false;
       this.backArrow = false;
     } 
-    this.scrollToElement();
     document.addEventListener('dragenter', function(e) {
      if (e.target.className == 'message-area' || e.target.className == 'messages-section' || e.target.className == 'v-list-item__content' || e.target.className == 'v-list-item__title' || e.target.className == 'v-list own-user message-list v-sheet theme--light v-list--two-line' || e.target.className == 'v-item-group theme--light v-list-item-group' || e.target.className == 'message-send-area' || e.target.className == 'row' || e.target.className == 'col-sm-10 col-md-10 col-12' || e.target.className == 'msg-text-box' ) {
         document.getElementById('dropzone').style.display = "block";
