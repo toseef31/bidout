4ac409976ea6361d20dc89b5472cba0a3724e209
peeknabeel5 <peeknabeel@gmail.com>
2022-11-03 16:46:30 +0500

    fixed location address

--- src/components/CompanyProfile/ServiceLocations.vue
+++ src/components/CompanyProfile/ServiceLocations.vue
@@ -6,10 +6,11 @@
       <div id="pac-container">
         <v-row>
           <v-col cols="10" sm="10">
-            <v-text-field placeholder="Add a location here ..." single-line outlined hide-details id="pac-input"></v-text-field>
+            <input type="text" class="location-input" name="" placeholder="Add a location here ..." id="pac-input">
+           <!--  <v-text-field placeholder="Add a location here ..." single-line outlined hide-details id="pac-input"></v-text-field> -->
           </v-col>
           <v-col cols="2" sm="2" class="pl-0 pt-0 pb-0">
-            <v-btn color="#0D9648" class="text-capitalize mr-2 white--text" width="100%" height="54px" @click="addLocation">Add</v-btn>
+            <v-btn color="#0D9648" class="text-capitalize mr-2 white--text" width="100%" height="54px" :loading="loading" :disabled="loading" @click="addLocation">Add</v-btn>
           </v-col>
         </v-row>
         <!-- <input id="pac-input" type="text" placeholder="Enter a location" /> -->
@@ -25,7 +26,7 @@
       <v-col cols="12" sm="12">
         <v-row>
           <v-col cols="12" class="-list text-left pt-2"  v-for="(location,index) in companyData.companyLocations">
-            <label class="d-flex justify-space-between"><span><v-icon>mdi-map-marker-outline</v-icon>{{location.location}}</span> <v-icon color="#F32349"  @click="deleteLocation(location)">mdi-trash-can-outline</v-icon></label>
+            <label class="d-flex justify-space-between"><span><v-icon>mdi-map-marker-outline</v-icon>{{location.location}}</span> <v-icon color="#F32349"   @click="deleteLocation(location,index)">mdi-trash-can-outline</v-icon></label>
           </v-col>
         </v-row>
       </v-col>
@@ -44,6 +45,8 @@ export default {
       lat: '',
       lng: '',
       address: "",
+      loading: false,
+      loader: null,
     };
   },
   computed:{
@@ -178,7 +181,7 @@ export default {
           }
           this.lat = place.geometry.location.lat();
           this.lng = place.geometry.location.lng();
-          this.address = place.formatted_address;
+          this.address = document.getElementById("pac-input").value;
         });
     },
     addLocation(){
@@ -192,8 +195,14 @@ export default {
         long: this.lng,
       }
       this.addCompanyLocation(data);
+      this.loader = 'loading';
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false,
+        document.getElementById("pac-input").value = '';
+      }, 5000)
     },
-    deleteLocation(data){
+    deleteLocation(data,indexing){
       var data = {
         id: data.id,
         companyId: this.$store.getters.userInfo.company.id,
@@ -201,7 +210,17 @@ export default {
         lat: data.lattitude,
         long: data.longitude,
       }
+      console.log(this.companyData.companyLocations);
+      const indexOfObject = this.companyData.companyLocations.findIndex(object => {
+        return object.id === data.id;
+      });
+
+      console.log(indexOfObject);
+
+      this.companyData.companyLocations.splice(indexOfObject, 1);
+    
       this.deleteCompanyLocation(data);
+      
     },
   },
   mounted() {
