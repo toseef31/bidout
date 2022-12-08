db33167c96de863f21c666761abf651a5e69b2ac
toseef31 <peekinternational@gmail.com>
2022-12-08 18:30:41 +0500

    edit template

--- src/router/bids.js
+++ src/router/bids.js
@@ -67,6 +67,18 @@ const routes = [
         },
       },
       {
+        path: "/edit-bid/:serial",
+        name: "NotCompleted",
+        component: () => import("@/views/Bid/EditBid.vue"),
+        beforeEnter: (to, from, next) => {
+          if (localStorage.getItem("userData") == null) {
+            next("/login");
+          } else {
+            next();
+          }
+        },
+      },
+      {
         path: "/completed",
         name: "Completed",
         component: () => import("@/views/Bid/Completed.vue"),
