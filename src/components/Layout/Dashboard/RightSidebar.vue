941e1e72a2fdfed13db0169d21ae620dce06843f
toseef31 <peekinternational@gmail.com>
2022-12-12 17:09:10 +0500

    map loading and right side bar issue fixed

--- src/components/Layout/Dashboard/RightSidebar.vue
+++ src/components/Layout/Dashboard/RightSidebar.vue
@@ -120,14 +120,14 @@ export default {
       return moment(date).startOf('hour').fromNow();
     },
   },
-  // async created(){
-  //   await this.getActivities(this.$store.getters.userInfo.id);
-  // },
-  updated(){
-
+  async created(){
+    await this.getActivities(this.$store.getters.userInfo.id);
+  },
+  async updated(){
+    // await this.getActivities(this.userDatas.id);
   },
   async mounted() {
-    await this.getActivities(this.userDatas.id);
+    // await this.getActivities(this.userDatas.id);
   }
 };
 </script>
--- src/views/Ofs/Company.vue
+++ src/views/Ofs/Company.vue
@@ -347,6 +347,14 @@ export default {
     },
   },
   async created(){
+    let mapScp = "map-api-script";
+    let mapAlreadyAttached = !!document.getElementById(mapScp);
+    if(!mapAlreadyAttached){
+     let mapScript = document.createElement('script')
+      mapScript.id = mapScp;
+     await mapScript.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key='+import.meta.env.VITE_GOOGLE_MAP+'&libraries=places')
+     document.head.appendChild(mapScript);
+    }
     // let mapScript = document.createElement('script')
     // await mapScript.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key='+import.meta.env.VITE_GOOGLE_MAP+'&libraries=places')
     // document.head.appendChild(mapScript);
