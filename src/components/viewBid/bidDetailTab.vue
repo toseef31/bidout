1a96e25d222ebdf08424d09ed71dbdfe38800f30
toseef31 <peekinternational@gmail.com>
2022-12-15 17:57:00 +0500

    truncate files name

--- src/components/viewBid/bidDetailTab.vue
+++ src/components/viewBid/bidDetailTab.vue
@@ -263,7 +263,7 @@
                 <td class="text-left">
                   <img :src="require('@/assets/images/bids/FilePdf.png')" />
                 </td>
-                <td class="text-left"><a :href="doc.url" target="_blank" class="text-decoration-none">{{ doc.fileName }}</a></td>
+                <td class="text-left d-block text-truncate pt-4" style="width:200px"><a :href="doc.url" target="_blank" class="text-decoration-none">{{ doc.fileName }}</a></td>
                 <td class="text-left">
                   <span>{{ doc.comment }}</span>
                 </td>
