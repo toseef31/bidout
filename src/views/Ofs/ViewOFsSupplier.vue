adafe0ba7fda80ed825f5c27bc3d9860eb5d0cc7
toseef31 <peekinternational@gmail.com>
2023-01-19 15:50:21 +0500

    ofs page width issue fixed

--- src/views/Ofs/ViewOFsSupplier.vue
+++ src/views/Ofs/ViewOFsSupplier.vue
@@ -167,16 +167,36 @@ export default {
             "breakpoint": 1280,
             "settings": {
               "slidesToShow": 3,
-              "slidesToScroll": 1,
+              "slidesToScroll": 3,
               "infinite": true,
               "dots": true
             }
           },
+          {
+              "breakpoint": 1024,
+              "settings": {
+                "slidesToShow": 2,
+                "slidesToScroll": 2,
+                "infinite": true,
+                "dots": true,
+                "arrows": true
+              }
+            },
             {
               "breakpoint": 950,
               "settings": {
                 "slidesToShow": 2,
                 "slidesToScroll": 2,
+                "infinite": true,
+                "dots": true,
+                "arrows": true
+              }
+            },
+            {
+              "breakpoint": 850,
+              "settings": {
+                "slidesToShow": 1,
+                "slidesToScroll": 1,
                 "infinite": true,
                 "dots": true,
                 "arrows": true
