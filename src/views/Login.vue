40ee7e4c3277364c6d50c0b2e31bbcf880fe53e7
toseef31 <peekinternational@gmail.com>
2023-03-14 10:55:18 +0500

    login email format foxed

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,71 +1,104 @@
 <template>
   <section class="section-container fill-height auth-module">
-     <v-row class="signin">
-       <v-col cols="12" md="6" class="left">
-          <div class="form-section mb-15">
-            <div class="logo mb-lg-15" :class="[passwordError != null ? 'mb-0' : 'mb-9']">
-              <router-link to="/"><img :src="require('@/assets/images/logo.png')" width="100%"></router-link>
+    <v-row class="signin">
+      <v-col cols="12" md="6" class="left">
+        <div class="form-section mb-15">
+          <div
+            class="logo mb-lg-15"
+            :class="[passwordError != null ? 'mb-0' : 'mb-9']"
+          >
+            <router-link to="/"
+              ><img :src="require('@/assets/images/logo.png')" width="100%"
+            /></router-link>
+          </div>
+          <v-alert type="error" v-if="logInError != null">
+            {{ logInError }}
+          </v-alert>
+
+          <v-alert type="error" v-if="passwordError != null">
+            Oops! You have entered a incorrect password, try again, if you are
+            still unsure of your password, please
+            <router-link to="/forgot-password" class="white--text"
+              >Reset Password</router-link
+            >
+          </v-alert>
+          <v-alert
+            type="success"
+            v-show="showSuccessAlert"
+            v-if="successMessage != ''"
+          >
+            {{ successMessage }}
+          </v-alert>
+          <v-form
+            @submit.prevent="loginRequest"
+            ref="form"
+            class="login-form"
+            v-model="valid"
+            lazy-validation
+          >
+            <label class="font-weight-bold">Email</label>
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              required
+              outlined
+              placeholder="example@bidout.app"
+              class="rounded-lg mt-2"
+              type="email"
+              color="#0D1139"
+              background-color="white"
+            ></v-text-field>
+            <label class="font-weight-bold">Password</label>
+            <v-text-field
+              v-model="password"
+              :rules="passwordRules"
+              :append-icon="
+                showPass ? 'mdi-eye view-pass' : 'mdi-eye-off view-pass'
+              "
+              @click:append="showPass = !showPass"
+              required
+              outlined
+              placeholder="••••••••••••"
+              class="rounded-lg mt-2"
+              :type="showPass ? 'text' : 'password'"
+              color="#0D1139"
+              background-color="white"
+            ></v-text-field>
+            <v-hover v-slot="{ hover }" open-delay="200">
+              <router-link to="/forgot-password" class="ml-3 font-weight-medium"
+                >Forgot password?</router-link
+              >
+            </v-hover>
+            <div class="text-center mt-8">
+              <v-btn
+                class="signin-btn rounded-lg font-weight-bold text-capitalize"
+                type="submit"
+                color="success"
+                :disabled="!valid"
+                :loading="loginLoading"
+                @click="login"
+              >
+                Submit
+              </v-btn>
             </div>
-            <v-alert type="error" v-if="logInError != null">
-              {{ logInError }}
-            </v-alert>
-
-            <v-alert type="error" v-if="passwordError != null">
-              Oops! You have entered a incorrect password, try again, if you are still unsure of your password, please <router-link to="/forgot-password" class="white--text">Reset Password</router-link>
-            </v-alert>
-            <v-alert type="success" v-show="showSuccessAlert" v-if="successMessage != ''">
-              {{ successMessage }}
-            </v-alert>
-            <v-form @submit.prevent="loginRequest" ref="form" class="login-form" v-model="valid"
-              lazy-validation>
-               <label class="font-weight-bold">Email</label>
-                 <v-text-field
-                   v-model="email"
-                   :rules="emailRules"
-                   required
-                   outlined
-                   placeholder="example@bidout.app"
-                   class="rounded-lg mt-2"
-                   type="email"
-                   color="#0D1139"
-                   background-color="white"
-                 ></v-text-field>
-                 <label class="font-weight-bold">Password</label>
-                 <v-text-field
-                   v-model="password"
-                   :rules="passwordRules"
-                   :append-icon="showPass ? 'mdi-eye view-pass' : 'mdi-eye-off view-pass'"
-                   @click:append="showPass = !showPass"
-                   required
-                   outlined
-                   placeholder="••••••••••••"
-                   class="rounded-lg mt-2"
-                   :type="showPass ? 'text' : 'password'"
-                   color="#0D1139"
-                   background-color="white"
-                 ></v-text-field>
-                 <v-hover
-                    v-slot="{ hover }"
-                    open-delay="200"
-                  >
-                 <router-link to="/forgot-password" class="ml-3 font-weight-medium">Forgot password?</router-link> </v-hover>
-               <div class="text-center mt-8">
-                 <v-btn class="signin-btn rounded-lg font-weight-bold text-capitalize" type="submit" color="success" :disabled="!valid" :loading="loginLoading" @click="login">
-                   Submit
-                 </v-btn>
-               </div>
-
-             </v-form>
-          </div>
-          <div class="bottom-section">
-            <p class="center font-weight-bold">Don't have account yet? <router-link to="/get-started">Join BidOut</router-link></p>
-          </div>
-       </v-col>
-       <v-col cols="12" md="6" class="right d-none d-md-block">
-         <v-img :src="require('@/assets/images/sideBanner.png')" width="100%" height="100%"></v-img>
-       </v-col>
-     </v-row>
-   </section>
+          </v-form>
+        </div>
+        <div class="bottom-section">
+          <p class="center font-weight-bold">
+            Don't have account yet?
+            <router-link to="/get-started">Join BidOut</router-link>
+          </p>
+        </div>
+      </v-col>
+      <v-col cols="12" md="6" class="right d-none d-md-block">
+        <v-img
+          :src="require('@/assets/images/sideBanner.png')"
+          width="100%"
+          height="100%"
+        ></v-img>
+      </v-col>
+    </v-row>
+  </section>
 </template>
 
 <script>
@@ -78,13 +111,13 @@ export default {
       valid: true,
       email: this.$store.getters.resetEmail,
       emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+        (v) => !!v || 'E-mail is required',
+        (v) => /^\w+([.+_-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid',
       ],
       password: '',
       passwordRules: [
-        v => !!v || 'Password is required',
-        v => (v && v.length >= 6) || 'Password must be more than 6 characters',
+        (v) => !!v || 'Password is required',
+        (v) => (v && v.length >= 6) || 'Password must be more than 6 characters',
       ],
       showPass: false,
       xhrRequest: false,
@@ -138,6 +171,4 @@ export default {
   },
 };
 </script>
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
