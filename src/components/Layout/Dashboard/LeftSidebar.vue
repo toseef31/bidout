9ddccdb3ad88352d7950b85405043b29f40098fc
toseef31 <peekinternational@gmail.com>
2023-04-12 21:09:25 +0500

    token action removed from leftsidebar

--- src/components/Layout/Dashboard/LeftSidebar.vue
+++ src/components/Layout/Dashboard/LeftSidebar.vue
@@ -294,7 +294,7 @@ export default {
       return false;
     },
     redirectLink(item) {
-      this.$router.push("/" + item.link).catch(() => {});
+      this.$router.push(`/${item.link}`).catch(() => {});
     },
     redirectCompany() {
       this.$router.push("/company-profile").catch(() => {});
