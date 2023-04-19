c2b9de91989dcefd22bf10cc6d172648213a9035
Mauro Soares <mauro@tempocerto.inf.br>
2023-04-19 14:48:15 -0300

    :art: /templates not showing creator and data fixed

--- src/views/Bid/Templates.vue
+++ src/views/Bid/Templates.vue
@@ -1,11 +1,23 @@
 <template>
   <v-row class="createBid-module pa-0 ma-0">
-    <v-col class="pa-0 pr-sm-3"
-      :class="[showSideBar ? 'col-md-12 col-12 col-sm-12' : 'mid-content-collapse', activityPanel ? 'd-sm-block' : 'd-md-block']"
-      v-show="!activityPanel">
+    <v-col
+      class="pa-0 pr-sm-3"
+      :class="[
+        showSideBar ? 'col-md-12 col-12 col-sm-12' : 'mid-content-collapse',
+        activityPanel ? 'd-sm-block' : 'd-md-block',
+      ]"
+      v-show="!activityPanel"
+    >
       <div class="mid-content">
-        <div class="content-section fill-height d-flex justify-center align-center" v-if="loading">
-          <v-progress-circular :width="3" color="green" indeterminate></v-progress-circular>
+        <div
+          class="content-section fill-height d-flex justify-center align-center"
+          v-if="loading"
+        >
+          <v-progress-circular
+            :width="3"
+            color="green"
+            indeterminate
+          ></v-progress-circular>
         </div>
         <div class="content-section fill-height" v-else>
           <v-row align="center" justify="space-between" class="px-6 mb-4 mt-2 mx-0">
@@ -13,49 +25,55 @@
               <h4>Templates</h4>
             </v-col>
             <v-col cols="6" class="text-right pr-0">
-              <p class="mb-0"><router-link to="/manage-templates">Manage Templates</router-link></p>
+              <p class="mb-0">
+                <router-link to="/manage-templates">Manage Templates</router-link>
+              </p>
             </v-col>
           </v-row>
           <v-simple-table class="template-table">
             <template v-slot:default>
               <thead>
                 <tr>
-                  <th class="text-left pl-6">
-                    Template Name
-                  </th>
-                  <th class="text-left">
-                    Type
-                  </th>
-                  <th class="text-left">
-                    Created Data
-                  </th>
-                  <th class="text-left">
-                    Creator
-                  </th>
-                  <th class="text-left">
-                    Notes
-                  </th>
-                  <th class="text-left pr-6">
-
-                  </th>
+                  <th class="text-left pl-6">Template Name</th>
+                  <th class="text-left">Type</th>
+                  <th class="text-left">Created Data</th>
+                  <th class="text-left">Creator</th>
+                  <th class="text-left">Notes</th>
+                  <th class="text-left pr-6"></th>
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="(template, index) in bidTemplates" class="py-4 px-6"
-                  v-if="template.companyId == userDatas.companyId">
+                <tr
+                  v-for="(template, index) in bidTemplates"
+                  class="py-4 px-6"
+                  v-if="template.companyId == userDatas.companyId"
+                >
                   <td class="text-left pl-6">{{ template.title }}</td>
                   <td class="text-left">{{ template.type }}</td>
-                  <td class="text-left">{{ formatDate(template.createdAt._seconds, template.createdAt._nanoseconds) }}
-                  </td>
-                  <td class="text-left">{{ template.userName ? template.userName : 'No name' }}</td>
                   <td class="text-left">
-                    <div class="text-truncate" style="width: 400px;">{{ template.note ? template.note : 'Note not added
-                    yet.' }}</div>
+                    {{ formatDatev2(template.dueDate) }} {{ template.dueTime }}
+                  </td>
+                  <td class="text-left">
+                    {{
+                      template.user
+                        ? `${template.user.firstName} ${template.user.lastName}`
+                        : "No name"
+                    }}
+                  </td>
+                  <td class="text-left">
+                    <div class="text-truncate" style="width: 400px">
+                      {{ template.note ? template.note : 'Note not added
+                    yet.' }}
+                    </div>
                   </td>
                   <td class="text-left pr-6">
-
-                    <v-btn color="#0D9648" elevation="0" class="white--text text-capitalize"
-                      @click="useTemplate(template)">Use Template</v-btn>
+                    <v-btn
+                      color="#0D9648"
+                      elevation="0"
+                      class="white--text text-capitalize"
+                      @click="useTemplate(template)"
+                      >Use Template</v-btn
+                    >
                   </td>
                 </tr>
               </tbody>
@@ -67,14 +85,14 @@
   </v-row>
 </template>
 <script>
-import { mapActions, mapGetters } from 'vuex';
-import moment from 'moment-timezone';
-import Navbar from '../../components/Layout/Navbar.vue';
-import LeftSidebar from '../../components/Layout/Dashboard/LeftSidebar.vue';
-import RightSidebar from '../../components/Layout/Dashboard/RightSidebar.vue';
+import { mapActions, mapGetters } from "vuex";
+import moment from "moment-timezone";
+import Navbar from "../../components/Layout/Navbar.vue";
+import LeftSidebar from "../../components/Layout/Dashboard/LeftSidebar.vue";
+import RightSidebar from "../../components/Layout/Dashboard/RightSidebar.vue";
 
 export default {
-  name: 'Template',
+  name: "Template",
   components: {
     Navbar,
     LeftSidebar,
@@ -83,12 +101,12 @@ export default {
 
   data() {
     return {
-      users: '',
-      templates: '',
+      users: "",
+      templates: "",
     };
   },
   computed: {
-    ...mapGetters(['bidTemplates']),
+    ...mapGetters(["bidTemplates"]),
     showSideBar() {
       return this.$store.getters.g_sideBarOpen;
     },
@@ -103,27 +121,34 @@ export default {
     },
   },
   methods: {
-    ...mapActions(['getBidTemplates']),
+    ...mapActions(["getBidTemplates"]),
     async useTemplate(template) {
-      this.$store.commit('setBidData', template);
-      this.$store.state.bid.bidData.statusType = 'templateBid';
-      await this.$store.dispatch('getTeamMembers', this.userDatas.company);
-      await this.$store.dispatch('getSalesReps', { query: '', basin: 'all' });
-      await this.$store.dispatch('getCategories');
-      await this.$store.dispatch('searchByCompany', { query: '', basin: 'all' });
-      this.$router.push('/create-bid/');
+      this.$store.commit("setBidData", template);
+      this.$store.state.bid.bidData.statusType = "templateBid";
+      await this.$store.dispatch("getTeamMembers", this.userDatas.company);
+      await this.$store.dispatch("getSalesReps", { query: "", basin: "all" });
+      await this.$store.dispatch("getCategories");
+      await this.$store.dispatch("searchByCompany", { query: "", basin: "all" });
+      this.$router.push("/create-bid/");
     },
     formatDate(item, item2) {
-      const date = moment(item * 1000 + item2 / 1000000).tz('America/Chicago').format('MM/DD/YYYY');
+      const date = moment(item * 1000 + item2 / 1000000)
+        .tz("America/Chicago")
+        .format("MM/DD/YYYY");
 
       return date;
+    },
+    formatDatev2(dueDate) {
+      return dueDate !== "" && dueDate !== null
+        ? moment.tz(dueDate, "America/Chicago").format("MM/DD/YYYY")
+        : "";
     },
   },
   async created() {
     await this.getBidTemplates();
   },
   mounted() {
-    document.title = 'Create Bid - BidOut';
+    document.title = "Create Bid - BidOut";
     this.users = this.$store.getters.userInfo;
   },
 };
