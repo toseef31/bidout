1e3127cbf5f414a4216c09157ff8249665416610
Teklewold Belayneh <teklewoldb9@gmail.com>
2023-04-20 21:10:08 +0300

    static data changed with dynamic data in existing supplier signup confirmation

--- src/components/SignUp/confirmation-existing.vue
+++ src/components/SignUp/confirmation-existing.vue
@@ -1,16 +1,17 @@
 <template>
-    <v-row justify="center">
-        <v-col cols="12" md="10">
-            <div class="alert-section mb-5 mt-14">
-                <v-img :src="require('@/assets/images/getStarted/warning.png')" max-width="120px"
-                    class="mx-auto mb-8"></v-img>
-                <p class="existing-title">An existing account already exists for <strong>{{ companyName }}</strong>.</p>
-                <p class="existing-title">A notification has been sent to the Administrator on the account to approve your
-                    access request. For immediate assistance, please reach out your team members at Patterson-UTI which
-                    include: <strong v-for="(admins,index) in companyAdmins" :key="index">{{admins.firstName}} {{admins.lastName}}<span v-if="index < companyAdmins.length - 1">, </span></strong>.</p>
-            </div>
-        </v-col>
-    </v-row>
+  <v-row justify="center">
+    <v-col cols="12" md="10">
+      <div class="alert-section mb-5 mt-14">
+        <v-img :src="require('@/assets/images/getStarted/warning.png')" max-width="120px" class="mx-auto mb-8"></v-img>
+        <p class="existing-title">An existing account already exists for <strong>{{ companyName }}</strong>.</p>
+        <p class="existing-title">A notification has been sent to the Administrator on the account to approve your
+          access request. For immediate assistance, please reach out your team members at <strong>{{ companyName
+          }}</strong> which
+          include: <strong v-for="(admins, index) in companyAdmins" :key="index">{{ admins.firstName }}
+            {{ admins.lastName }}<span v-if="index < companyAdmins.length - 1">, </span></strong>.</p>
+      </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
