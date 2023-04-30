b3b7c089cadff1542aa2d0285c013189b03b2a15
Teklewold Belayneh <teklewoldb9@gmail.com>
2023-04-30 20:55:37 +0300

    sentry session replay added

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import Vuex from 'vuex';
-import LogRocket from 'logrocket';
-import createPlugin from 'logrocket-vuex';
-import * as Sentry from '@sentry/vue';
-import { BrowserTracing } from '@sentry/tracing';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import axios from 'axios';
-import vueCountryRegionSelect from 'vue-country-region-select';
-import VueMoment from 'vue-moment';
-import moment from 'moment-timezone';
-import VueSignaturePad from 'vue-signature-pad';
-import '@/assets/styles/index.scss';
-import VueCroppie from 'vue-croppie';
-import 'croppie/croppie.css';
-import VueGtag from 'vue-gtag';
-import VueMeta from 'vue-meta';
-import Toasted from 'vue-toasted';
-import vuetify from './plugins/vuetify';
-import store from './store';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Vuex from "vuex";
+import LogRocket from "logrocket";
+import createPlugin from "logrocket-vuex";
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import axios from "axios";
+import vueCountryRegionSelect from "vue-country-region-select";
+import VueMoment from "vue-moment";
+import moment from "moment-timezone";
+import VueSignaturePad from "vue-signature-pad";
+import "@/assets/styles/index.scss";
+import VueCroppie from "vue-croppie";
+import "croppie/croppie.css";
+import VueGtag from "vue-gtag";
+import VueMeta from "vue-meta";
+import Toasted from "vue-toasted";
+import vuetify from "./plugins/vuetify";
+import store from "./store";
 import "./plugins/vee-validate";
 
 // Your web app's Firebase configuration
@@ -38,7 +38,9 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
-axios.defaults.headers.common.Authorization = `Bearer ${JSON.parse(localStorage.getItem('token'))}`;
+axios.defaults.headers.common.Authorization = `Bearer ${JSON.parse(
+  localStorage.getItem("token")
+)}`;
 
 createPlugin(LogRocket);
 
@@ -48,10 +50,10 @@ Vue.use(VueMoment, {
   moment,
 });
 Vue.use(VueMeta, {
-  keyName: 'metaInfo',
-  attribute: 'data-vue-meta',
-  ssrAttribute: 'data-vue-meta-server-rendered',
-  tagIDKeyName: 'vmid',
+  keyName: "metaInfo",
+  attribute: "data-vue-meta",
+  ssrAttribute: "data-vue-meta-server-rendered",
+  tagIDKeyName: "vmid",
   refreshOnceOnNavigation: true,
 });
 // Vue.use(momentTimeZone);
@@ -59,25 +61,47 @@ Vue.use(VueSignaturePad);
 Vue.use(VueCroppie);
 Vue.use(Toasted);
 
-if (process.env.NODE_ENV === 'production') {
-  LogRocket.init('voayxx/v2-ib4bb');
+Sentry.init({
+  Vue,
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: [import.meta.env.VITE_PROJECT_URL, /^\//],
+    }),
+    new Sentry.Replay({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+});
+
+if (process.env.NODE_ENV === "production") {
+  LogRocket.init("voayxx/v2-ib4bb");
 
   Sentry.init({
     Vue,
     dsn: import.meta.env.VITE_SENTRY_DSN,
+    replaysSessionSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
     integrations: [
       new BrowserTracing({
         routingInstrumentation: Sentry.vueRouterInstrumentation(router),
         tracingOrigins: [import.meta.env.VITE_PROJECT_URL, /^\//],
       }),
+      new Sentry.Replay({
+        maskAllText: false,
+        blockAllMedia: false,
+      }),
     ],
-
   });
 }
 
 LogRocket.getSessionURL((sessionURL) => {
   Sentry.configureScope((scope) => {
-    scope.setExtra('sessionURL', sessionURL);
+    scope.setExtra("sessionURL", sessionURL);
   });
 });
 
@@ -85,10 +109,9 @@ Vue.use(VueGtag, {
   config: { id: import.meta.env.VITE_GOOGLE_TAG_MANAGER },
 });
 
-
 new Vue({
   vuetify,
   router,
   store,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
