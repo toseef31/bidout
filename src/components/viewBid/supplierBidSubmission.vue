6e3aa33ca66ca4d55612cdf4ea4d1b9e5c2a75c3
toseef31 <peekinternational@gmail.com>
2023-05-30 14:59:11 +0500

    doc and excel icon added

--- src/components/viewBid/supplierBidSubmission.vue
+++ src/components/viewBid/supplierBidSubmission.vue
@@ -224,7 +224,8 @@
                     <img :src="require('@/assets/images/bids/FilePdf.png')"
                       v-if="checkFileType(doc.fileName) === 'pdf'" />
                     <img :src="require('@/assets/images/bids/FileDoc.png')"
-                      v-else-if="checkFileType(doc.fileName) === 'docx'" />
+                      v-else-if="checkFileType(doc.fileName) === 'docx' || checkFileType(doc.fileName) === 'doc'" />
+                      <v-icon color="#0D1139" v-else-if="checkFileType(doc.fileName) === 'xlsx' || checkFileType(doc.fileName) === 'xls'">mdi-file-excel-outline</v-icon>
                     <v-icon color="#0D1139" v-else>mdi-file-document</v-icon>
                   </td>
                   <td class="text-left">{{ doc.fileName }}</td>
